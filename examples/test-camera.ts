551c1d0cca2eea30ca5dc81003e6b9cc2e212c17
Alex Eden <alexandereden91@gmail.com>
2019-06-21 15:44:35 -0500

    Setup a test script showing camera frames

--- examples/test-camera.ts
+++ examples/test-camera.ts
@@ -1,1 +1,15 @@
-console.log(`I'm the test script!`);
+import * as cv from 'opencv4nodejs';
+
+(async () => {
+  const cap = new cv.VideoCapture(0);
+
+  const showFrame = async () => {
+    const frame = await cap.readAsync();
+    cv.imshow('Test frame', frame);
+    cv.waitKey();
+    showFrame();
+  };
+
+  showFrame();
+
+})();
